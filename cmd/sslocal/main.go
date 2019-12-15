b38b2d56fbd814b8cfef05093d7572891bbf0ba6
yang <yangsoonlx@gmail.com>
2019-12-15 21:47:19 +0800

    :construction: 添加日志并完成sslocal骨架

--- cmd/sslocal/main.go
+++ cmd/sslocal/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"flag"
+	ss "github.com/yangsoon/soonsocks"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+
+}
+
+func main() {
+	var configPath string
+	flag.StringVar(&configPath, "c", "config.json", "json file with config")
+	flag.Parse()
+
+	config, err := ss.ParseConfig(configPath)
+	if err != nil {
+		ss.Logger.Fatalf("parse %s failed %v \n", configPath, err)
+	}
+	ss.Logger.Printf("config info: " +
+		"\n--------------------------------\n" +
+		"LocalAddr: %v\nServerAddr: %v\nMethod: %v" +
+		"\n--------------------------------\n",
+		config.LocalAddr,
+		config.ServerAddr,
+		config.Method)
+
+	l, err := net.Listen("tcp", config.LocalAddr)
+	if err != nil {
+		ss.Logger.Printf("SSLocal listen faild %v\n", err)
+		panic(err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			ss.Logger.Printf("SSLocal accept client error: %v\n", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
--- util.go
+++ util.go
@@ -1,1 +1,9 @@
 package soonsocks
+
+import (
+	"log"
+	"os"
+)
+
+var Logger = log.New(os.Stdout, "SSLogger: ", log.Ldate | log.Ltime)
+
